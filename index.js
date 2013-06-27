0d6d36c4c6ff8e683f5ad7e44798e123b27cb8a7
PG <pgherveou@gmail.com>
2013-06-27 12:00:02 -0700

    release paragraph element

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
 
 var prefixes = ['webkit','Moz','ms','O']
   , len = prefixes.length
-  , test = document.createElement('p')
+  , p = document.createElement('p')
+  , style = p.style
   , capitalize = function (str) {return str.charAt(0).toUpperCase() + str.slice(1);}
   , dasherize = function(str) {
       return str.replace(/([A-Z])/g, function(str,m1) {
         return '-' + m1.toLowerCase();
       });
     };
+
+// nullify p to release dom node
+p = null;
 
 /**
  * get prefix for dom style
@@ -27,7 +31,7 @@ module.exports = function(ppty, dasherized) {
   var Ppty, name, Name;
 
   // test without prefix
-  if (test.style[ppty] !== undefined) {
+  if (style[ppty] !== undefined) {
     if (!dasherized) return ppty;
     return dasherize(ppty);
   }
@@ -36,7 +40,7 @@ module.exports = function(ppty, dasherized) {
   Ppty = capitalize(ppty);
   for (i = 0; i < len; i++) {
     name = prefixes[i] + Ppty;
-    if (test.style[name] !== undefined) {
+    if (style[name] !== undefined) {
       if (!dasherized) return name;
       return '-' + prefixes[i].toLowerCase() + '-' + dasherize(ppty);
     }
